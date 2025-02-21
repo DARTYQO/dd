5db87cc8c1090b349624f9921d2a449ca708d384
Generous programmer <ch05341315656@GMAIL.COM>
2025-02-21 03:30:01 +0200

    Update download_file.py

--- download_file.py
+++ download_file.py
@@ -33,6 +33,6 @@ commit_message = "Add downloaded file"
 branch_name = "main"
 
 upload_file_to_github(repo_name, file_path, commit_message, branch_name)
-    url = 'https://mitmachim.top/assets/uploads/files/1586507580718-%D7%92%D7%99%D7%9E%D7%98%D7%A8%D7%99%D7%95%D7%9F-%D7%9C%D7%A9%D7%9E%D7%97%D7%95%D7%AA-1.3.rar'
-    filename = "downloaded_file"
-    download_file(url, filename)
+url = 'https://mitmachim.top/assets/uploads/files/1586507580718-%D7%92%D7%99%D7%9E%D7%98%D7%A8%D7%99%D7%95%D7%9F-%D7%9C%D7%A9%D7%9E%D7%97%D7%95%D7%AA-1.3.rar'
+filename = "downloaded_file"
+download_file(url, filename)
