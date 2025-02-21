112f75e3fed5074d44e70cf21b72a3630a4941ab
Generous programmer <ch05341315656@GMAIL.COM>
2025-02-21 02:54:36 +0200

    Update download_file.py

--- download_file.py
+++ download_file.py
@@ -7,6 +7,6 @@ def download_file(url, filename):
         file.write(response.content)
 
 if __name__ == "__main__":
-    url = os.getenv('https://mitmachim.top/assets/uploads/files/1586507580718-%D7%92%D7%99%D7%9E%D7%98%D7%A8%D7%99%D7%95%D7%9F-%D7%9C%D7%A9%D7%9E%D7%97%D7%95%D7%AA-1.3.rar')
+    url = 'https://mitmachim.top/assets/uploads/files/1586507580718-%D7%92%D7%99%D7%9E%D7%98%D7%A8%D7%99%D7%95%D7%9F-%D7%9C%D7%A9%D7%9E%D7%97%D7%95%D7%AA-1.3.rar'
     filename = "downloaded_file"
     download_file(url, filename)
